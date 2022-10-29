f19145d62aa287676609c3123f0f2e400d79f49a
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2022-10-29 20:54:34 +0800

    Unbreak CI? 7

--- download_libraries.sh
+++ download_libraries.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/STMicroelectronics/STM32CubeF7 --depth=1 --no-checkout
 cd STM32CubeF7/
 git checkout master Drivers/CMSIS/ Drivers/STM32F7xx_HAL_Driver/ Projects/STM32F746ZG-Nucleo/Templates/SW4STM32/STM32F746ZG_Nucleo_ITCM-FLASH/
+rm Drivers/STM32F7xx_HAL_Driver/Src/*_template.c
 cd ..
