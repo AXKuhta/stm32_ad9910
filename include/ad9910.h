579fa2114801661b3d112aecdb8cff48cac8d255
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-06-21 11:27:42 +0800

    Designate some sections in ad9910.h

--- include/ad9910.h
+++ include/ad9910.h
@@ -1,11 +1,15 @@
 
+// GPIO
 uint16_t profile_to_gpio_states(uint8_t profile_id);
 void set_profile(uint8_t profile_id);
 void set_ramp_direction(uint8_t direction);
 void ad_pulse_io_update();
+
+// SPI
 void ad_readback_all();
 void ad_write_all();
-void ad_enable_amplitude_scaler();
+
+// Профили
 void ad_set_profile_freq(int profile_id, uint32_t freq_hz);
 void ad_set_profile_amplitude(int profile_id, uint16_t amplitude);
 void ad_set_profile_phase(int profile_id, uint16_t phase);
@@ -13,18 +17,24 @@ void ad_set_ram_profile(int profile_id, uint16_t step_rate, uint16_t start, uint
 void ad_set_ram_freq(uint32_t freq_hz);
 void ad_set_ram_phase(uint16_t phase);
 void ad_set_ram_amplitude(uint16_t amplitude);
-void ad_ram_test();
+
+// DRG
 void ad_enable_ramp();
 void ad_disable_ramp();
 void ad_set_ramp_limits(uint32_t lower_hz, uint32_t upper_hz);
 void ad_set_ramp_step(uint32_t decrement, uint32_t increment);
 void ad_set_ramp_rate(uint16_t down_rate, uint16_t up_rate);
+
+// RAM
 void ad_enable_ram();
 void ad_disable_ram();
 void ad_ram_destination_freq();
 void ad_ram_destination_phase();
 void ad_ram_destination_amplitude();
 void ad_ram_destination_polar();
+void ad_ram_test();
+
+// Прочее
 void ad_drop_phase_static_reset();
 void ad_init();
 
