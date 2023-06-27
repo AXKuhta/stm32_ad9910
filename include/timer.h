AXKuhta/stm32_ad9910

commit f4b0cfeabf39419513bf5593b2b3a4ec25b19d40

Trigger TIM8 from TIM2 CH3 specifically
Switch TIM2 CH3 to PWM2 from TOGGLE (Toggle worked too but it skipped half of EXT TRIGs)

timer.c
@@ -84,8 +84,8 @@ void timer2_init() {
 	timer2 = timer2_defaults;
 
 	TIM_OC_InitTypeDef oc_config = {
-		.OCMode = TIM_OCMODE_TOGGLE,
-		.Pulse = 50,
+		.OCMode = TIM_OCMODE_PWM2,
+		.Pulse = 0,
 		.OCPolarity = TIM_OCPOLARITY_HIGH,
 		.OCFastMode = TIM_OCFAST_DISABLE
 	};
@@ -101,6 +101,13 @@ void timer2_init() {
 	};
 	
 	HAL_TIM_SlaveConfigSynchronization(&timer2, &slave_config);
+
+	TIM_MasterConfigTypeDef master_config = {
+		.MasterOutputTrigger = TIM_TRGO_OC3REF,
+		.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE
+	};
+
+	HAL_TIMEx_MasterConfigSynchronization(&timer2, &master_config);
 	
 	HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
 	HAL_NVIC_EnableIRQ(TIM2_IRQn);

commit a73358b1e50ee724a40ceda9d44817e2a880b03d

Fix pulses with offset == 0, been broken since f4b0cfeabf

include/timer.h
@@ -6,6 +6,8 @@
 #define MAX_NS_16BIT_216MHz ((1 << 16) - 1) / NS_TO_216MHZ_MU
 
 void timer2_init();
+void timer2_trgo_on_ch3();
+void timer2_trgo_on_reset();
 void timer2_stop();
 void timer2_restart();
 

sequencer.c
@@ -72,6 +72,14 @@ void pulse_complete_callback() {
 	spi_write_entry(entry);
 	ad_drop_phase_static_reset();
 
+	// Если t1 == 0, то TIM2 CH3 не сможет сгенерировать событие для запуска таймера модуляции
+	// Пересесть на TIM2 RESET в таких ситуациях
+	if (entry.t1 > 0) {
+		timer2_trgo_on_ch3();
+	} else {
+		timer2_trgo_on_reset();
+	}
+
 	// Можно производить запись только в верхнюю часть регистра ODR, сдвинув адрес на 1
 	// DMA работает в режиме DMA_CIRCULAR, модуляция будет идти по кругу
 	HAL_DMA_Start(&dma_timer8_up, (uint32_t)profile_mod_buffer, (uint32_t)&GPIOD->ODR + 1, profile_mod_size);

timer.c
@@ -101,12 +101,6 @@ void timer2_init() {
 	};
 	
 	HAL_TIM_SlaveConfigSynchronization(&timer2, &slave_config);
-
-	TIM_MasterConfigTypeDef master_config = {
-		.MasterOutputTrigger = TIM_TRGO_OC3REF
-	};
-
-	HAL_TIMEx_MasterConfigSynchronization(&timer2, &master_config);
 	
 	HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
 	HAL_NVIC_EnableIRQ(TIM2_IRQn);
@@ -114,6 +108,16 @@ void timer2_init() {
 	// Запуск требуется даже при настроенном триггере
 	HAL_TIM_OC_Start(&timer2, TIM_CHANNEL_3);
 	HAL_TIM_OC_Start_IT(&timer2, TIM_CHANNEL_4);
+}
+
+// Обычный режим, отступ > 0
+void timer2_trgo_on_ch3() {
+	HAL_TIMEx_MasterConfigSynchronization(&timer2, &(TIM_MasterConfigTypeDef){ .MasterOutputTrigger = TIM_TRGO_OC3REF });
+}
+
+// Особый режим для ситуаций, когда отступ = 0
+void timer2_trgo_on_reset() {
+	HAL_TIMEx_MasterConfigSynchronization(&timer2, &(TIM_MasterConfigTypeDef){ .MasterOutputTrigger = TIM_TRGO_RESET });
 }
 
 void timer2_stop() {	