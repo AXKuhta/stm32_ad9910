797968aa1c3f81fd9f271118c872ab6e2e528339
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-07-04 11:40:37 +0800

    Move AD9910 RAM functions into ad9910/ram.c

--- include/ad9910.h
+++ include/ad9910.h
@@ -18,25 +18,10 @@ void ad_set_ram_freq(uint32_t ftw);
 void ad_set_ram_phase(uint16_t pow);
 void ad_set_ram_amplitude(uint16_t asf);
 
-// RAM
-#define AD_RAM_DESTINATION_FREQ 		0
-#define AD_RAM_DESTINATION_PHASE 		1 // Не будет работать, пока выставлен бит Enable amplitude scale from single tone profiles (так как амплитуда будет нулевая)
-#define AD_RAM_DESTINATION_AMPLITUDE 	2
-#define AD_RAM_DESTINATION_POLAR 		3
-
-#define AD_RAM_PROFILE_MODE_DIRECTSWITCH 0b00000000
-#define AD_RAM_PROFILE_MODE_ZEROCROSSING 0b00001000 // Если включен zero-crossing, то режим обязательно direct switch
-
-void ad_enable_ram();
-void ad_disable_ram();
-void ad_set_ram_destination(uint8_t destination);
-void ad_write_ram(uint32_t* buffer, size_t count);
-void ad_read_ram(uint32_t* buffer, size_t count);
-void ad_ram_test();
-
 // Прочее
 void ad_drop_phase_static_reset();
 void ad_init();
 
 #include "ad9910/drg.h"
 #include "ad9910/units.h"
+#include "ad9910/ram.h"
---
+++ include/ad9910/ram.h
@@ -0,0 +1,16 @@
+
+// RAM
+#define AD_RAM_DESTINATION_FREQ 		0
+#define AD_RAM_DESTINATION_PHASE 		1 // Не будет работать, пока выставлен бит Enable amplitude scale from single tone profiles (так как амплитуда будет нулевая)
+#define AD_RAM_DESTINATION_AMPLITUDE 	2
+#define AD_RAM_DESTINATION_POLAR 		3
+
+#define AD_RAM_PROFILE_MODE_DIRECTSWITCH 0b00000000
+#define AD_RAM_PROFILE_MODE_ZEROCROSSING 0b00001000 // Если включен zero-crossing, то режим обязательно direct switch
+
+void ad_enable_ram();
+void ad_disable_ram();
+void ad_set_ram_destination(uint8_t destination);
+void ad_write_ram(uint32_t* buffer, size_t count);
+void ad_read_ram(uint32_t* buffer, size_t count);
+void ad_ram_test();
