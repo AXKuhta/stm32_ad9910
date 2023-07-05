661ed6447c15ee4bcfde74ab8a5072782485cfd8
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-07-05 14:21:22 +0800

    Drop unused prototypes from ad9910/profiles.h

--- include/ad9910/profiles.h
+++ include/ad9910/profiles.h
@@ -4,6 +4,3 @@ void ad_set_profile_freq(int profile_id, uint32_t ftw);
 void ad_set_profile_amplitude(int profile_id, uint16_t pow);
 void ad_set_profile_phase(int profile_id, uint16_t asf);
 void ad_set_ram_profile(int profile_id, uint16_t step_rate, uint16_t start, uint16_t end, uint8_t mode);
-void ad_set_ram_freq(uint32_t ftw);
-void ad_set_ram_phase(uint16_t pow);
-void ad_set_ram_amplitude(uint16_t asf);
