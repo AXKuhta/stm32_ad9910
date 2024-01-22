0cbb8fc8f2f8eda3758ec93aaeadf0e446517cc5
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-01-22 14:50:26 +0800

    Improved sweep calculation (untested)

--- algos.c
+++ algos.c
@@ -67,7 +67,89 @@ sweep_t calculate_sweep(uint32_t f1_hz, uint32_t f2_hz, uint32_t time_ns) {
 	};
 }
 
+#define ABS(X) (X < 0 ? -X : X)
+
+// Перебор различных шаговых интервалов DRG для поиска интервала с наименьшей ошибкой итоговой ширины свипа
+static uint16_t search_sweep_step_intervals(uint32_t time_ns, uint32_t delta_hz) {
+	double fstep = (ad_system_clock + 0.0) / (1ull << 32);
+	uint32_t clocks = time_ns / 4;
+	uint32_t overhead = 1;
+
+	if (time_ns % 4) {
+		printf("Not a factor of 4, unable to represent\n");
+		return 0;
+	}
+
+	uint32_t min_overhead = 1;
+	double min_error = ad_calc_ftw(delta_hz / clocks) * fstep * clocks - delta_hz;
+
+	while (1) {
+		if (clocks % overhead) {
+			// Нет возможности точно подогнать длительность
+			// Шаг пропускается
+		} else {
+			uint32_t steps = clocks/overhead;
+			double req_fstep = (delta_hz + 0.0) / steps;
+			uint32_t step_ftw = ad_calc_ftw(req_fstep);
+
+			double f_error = step_ftw*fstep*steps - delta_hz;
+
+			printf("%lu ns\t%lf Hz error\n", overhead*4, f_error);
+
+			if (ABS(f_error) < ABS(min_error)) {
+				min_overhead = overhead;
+				min_error = f_error;
+			}
+		}
+
+		overhead++;
+
+		if (overhead > 4) {
+			break;
+		}
+	}
+
+	printf("Min overhead: %lu\n", min_overhead);
+	printf("Min error: %lf\n", min_error);
+
+	return min_overhead;
+}
+
+// Вычислить параметры для свипа с частоты f1 до f2 за указанное время
+// Улучшенная версия
+sweep_t calculate_sweep_v2(uint32_t f1_hz, uint32_t f2_hz, uint32_t time_ns) {
+	uint32_t lower_ftw = 0;
+	uint32_t upper_ftw = 0;
+	uint32_t delta_hz = 0;
+
+	// Если конечная частота выше начальной частоты, то всё нормально
+	// Если же конечная частота НИЖЕ начальной частоты, то нужно использовать "зеркальные" значения частоты
+	// В даташите это называется "aliased image"
+	if (f2_hz > f1_hz) {
+		lower_ftw = ad_calc_ftw(f1_hz);
+		upper_ftw = ad_calc_ftw(f2_hz);
+		delta_hz = f2_hz - f1_hz;
+	} else {
+		lower_ftw = ad_calc_ftw(ad_system_clock - f1_hz);
+		upper_ftw = ad_calc_ftw(ad_system_clock - f2_hz);
+		delta_hz = f1_hz - f2_hz;
+	}
+
+	printf("Delta: %ld Hz\n", delta_hz);
+
+	uint16_t interval = search_sweep_step_intervals(time_ns, delta_hz);
+	uint32_t steps = (time_ns / 4) / interval;
+
+	return (sweep_t){
+		.lower_ftw = lower_ftw,
+		.upper_ftw = upper_ftw,
+		.fstep_ftw = ad_calc_ftw(delta_hz / steps),
+		.tstep = interval
+	};
+}
+
 // Представить некоторое время в наносекундах используя интервал шага * некоторое количество шагов оперативной памяти AD9910
+// TODO: учитывать ad_system_clock, сейчас предполагается 1 ГГц
 uint16_t fit_time(uint32_t time_ns) {
 	uint32_t clocks = time_ns / 4;
 	uint32_t overhead = 1;
