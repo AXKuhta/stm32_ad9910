5391c59528ccc39590f772c765275a5cdcd1146a
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-07-06 14:34:27 +0800

    (1/2) Upgrade to HAL v1.17.3

--- redownload_stm32_support_code.sh
+++ redownload_stm32_support_code.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/STMicroelectronics/STM32CubeF7 --branch=v1.17.1 --depth=1 --no-checkout
+git clone https://github.com/STMicroelectronics/STM32CubeF7/ --branch=v1.17.3 --depth=1
+rm -r STM32CubeF7/*
 cd STM32CubeF7/
-git checkout v1.17.1 Drivers/CMSIS/Core/Include/ Drivers/CMSIS/Device/ST/STM32F7xx/ Drivers/STM32F7xx_HAL_Driver/
-rm Drivers/STM32F7xx_HAL_Driver/Src/*_template.c
-cd ..
+git checkout Drivers/CMSIS/Core
+git submodule update --init Drivers/STM32F7xx_HAL_Driver Drivers/CMSIS/Device/ST/STM32F7xx
+
+# Убрать несобирающиеся файлы-шаблоны
+rm -vf Drivers/STM32F7xx_HAL_Driver/Src/*_template.c
